e79ca5eba0184099d3d50acaa879aef79656353e
Tauri <tauri.saar@voco.ee>
2023-02-07 20:35:44 +0200

    Displayed an article by slug

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+
+// add template engine
 const hbs = require('express-handlebars')
 
+// setup template engine directory and files extensions
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 app.engine('hbs', hbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
-    layoutsDir: path.join(__dirname, 'views/layouts'),
+    layoutsDir: path.join(__dirname, '/views/layouts'),
 }))
+
+// setup static public directory
 app.use(express.static('public'))
+
 const mysql = require('mysql')
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
+
+// create database connection
 var con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -25,18 +34,32 @@ con.connect(function (err) {
     console.log('Connected to joga_mysql db')
 })
 
+// show all articles - index page
 app.get("/", (req, res) => {
     let query = 'SELECT * FROM article';
     let articles = []
     con.query(query, (err, result) => {
         if (err) throw err
         articles = result
-        res.render('layouts/index.hbs', {
+        res.render('index.hbs', {
             articles: articles
         })
     })
 });
 
+// show article by this slug
+app.get("/article/:slug", (req, res) => {
+    let query = `SELECT * FROM article WHERE slug="${req.params.slug}"`
+    let article
+    con.query(query, (err, result) => {
+        if (err) throw err
+        article = result
+        //console.log(article)
+        res.render("article", {
+            article: article
+        })
+    })
+});
 
 app.listen(4000, () => {
     console.log('App is started at http://localhost:4000')
