a5a91db97b6c212000919d4c2909cc8808095b8b
Tauri <tauri.saar@voco.ee>
2023-02-07 19:24:34 +0200

    Displayed all articles

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-// application packages
 const express = require('express')
 const app = express()
-
 const path = require('path')
-//add template engine
-const hbs = require('express-handlebars');
-//setup template engine directory and files extensions
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+const hbs = require('express-handlebars')
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 app.engine('hbs', hbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
-    layoutsDir: __dirname + '/views/layouts/',
+    layoutsDir: path.join(__dirname, 'views/layouts'),
 }))
-//setup static public directory
-app.use(express.static('public'));
-
-
+app.use(express.static('public'))
 const mysql = require('mysql')
-
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
-
-// create database connection
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "qwerty",
-    database: "joga_mysql"
+    host: 'localhost',
+    user: 'root',
+    password: 'qwerty',
+    database: 'joga_mysql'
 })
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected to joga_mysql db");
+con.connect(function (err) {
+    if (err) throw err
+    console.log('Connected to joga_mysql db')
 })
 
-// app start point
-app.listen(3000, () => {
-    console.log('App is started at http://localhost:3000');
-    });
+app.get("/", (req, res) => {
+    let query = 'SELECT * FROM article';
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err
+        articles = result
+        res.render('layouts/index.hbs', {
+            articles: articles
+        })
+    })
+});
+
+
+app.listen(4000, () => {
+    console.log('App is started at http://localhost:4000')
+})
